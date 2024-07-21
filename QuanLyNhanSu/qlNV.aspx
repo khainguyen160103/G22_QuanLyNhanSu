1eab4fcb1cd00b1f99b621a81a98aee6f17a2c87
TusnCoder19 <21a100100409@students.hou.edu.vn>
2024-07-21 22:43:35 +0700

    qlnv

---
+++ QuanLyNhanSu/qlNV.aspx
@@ -0,0 +1,110 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="qlNV.aspx.cs" Inherits="qlNV.qlNV" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Quản lý nhân viên</title>
+    <link href="qlNV.css" rel="stylesheet" type="text/css" />
+    <script src="qlNV.js" type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <div class="container">
+            <h1 class="header">Quản lý nhân viên</h1>
+            <div class="header2">
+                <div class="form-group">
+                    <label for="txtMaNhanVien">Mã nhân viên:</label>
+                    <asp:TextBox ID="txtMaNhanVien" runat="server" CssClass="form-control1"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label for="imgNhanVien">Ảnh nhân viên:</label>
+                    <asp:FileUpload ID="imgNhanVien" runat="server" CssClass="form-control1" OnChange="previewImage()" />
+                    <asp:Image ID="previewImg" runat="server" Style="display:none;" />
+                </div>
+            </div>
+            <div class="row">
+                <!-- Cột 1 -->
+                <div class="column">
+                    <div class="form-group">
+                        <label for="txtTenNhanVien">Tên nhân viên:</label>
+                        <asp:TextBox ID="txtTenNhanVien" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="ddlGioiTinh">Giới tính:</label>
+                        <asp:DropDownList ID="ddlGioiTinh" runat="server" CssClass="form-control">
+                            <asp:ListItem Text="Nam" Value="Nam"></asp:ListItem>
+                            <asp:ListItem Text="Nữ" Value="Nữ"></asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtCCCD">CCCD:</label>
+                        <asp:TextBox ID="txtCCCD" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtNgayVaoLam">Ngày vào làm:</label>
+                        <asp:TextBox ID="txtNgayVaoLam" runat="server" CssClass="form-control"></asp:TextBox>
+                        <asp:CalendarExtender ID="ceNgayVaoLam" runat="server" TargetControlID="txtNgayVaoLam" Format="dd/MM/yyyy"></asp:CalendarExtender>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtPhongBan">Phòng ban:</label>
+                        <asp:TextBox ID="txtPhongBan" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="ddlTinhTrang">Tình trạng:</label>
+                        <asp:DropDownList ID="ddlTinhTrang" runat="server" CssClass="form-control">
+                            <asp:ListItem Text="Đang làm việc" Value="Đang làm việc"></asp:ListItem>
+                            <asp:ListItem Text="Nghỉ việc" Value="Nghỉ việc"></asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+                </div>
+
+                <!-- Cột 2 -->
+                <div class="column">
+                    <div class="form-group">
+                        <label for="txtDiaChi">Địa chỉ:</label>
+                        <asp:TextBox ID="txtDiaChi" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtSDT">SĐT:</label>
+                        <asp:TextBox ID="txtSDT" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtEmail">Email:</label>
+                        <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtChucVu">Chức vụ:</label>
+                        <asp:TextBox ID="txtChucVu" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label for="txtLuong">Lương:</label>
+                        <asp:TextBox ID="txtLuong" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                </div>
+
+                <!-- Cột 3 -->
+                <div class="column">
+                    <asp:GridView ID="gvNhanVien" runat="server" AutoGenerateColumns="False" CssClass="table" OnSelectedIndexChanged="gvNhanVien_SelectedIndexChanged" AutoGenerateSelectButton="True">
+                        <Columns>
+                            <asp:BoundField DataField="MaNhanVien" HeaderText="Mã nhân viên" />
+                            <asp:BoundField DataField="TenNhanVien" HeaderText="Tên nhân viên" />
+                            <asp:BoundField DataField="ChucVu" HeaderText="Chức vụ" />
+                            <asp:BoundField DataField="TinhTrang" HeaderText="Tình trạng" />
+                            <asp:CommandField ShowSelectButton="True" />
+                        </Columns>
+                    </asp:GridView>
+                </div>
+            </div>
+            <div class="buttons">
+                <asp:Button ID="btnThem" runat="server" Text="Thêm" CssClass="button" OnClick="btnThem_Click" />
+                <asp:Button ID="btnCapNhat" runat="server" Text="Cập nhật" CssClass="button" OnClick="btnCapNhat_Click" />
+                <asp:Button ID="btnXoa" runat="server" Text="Xóa" CssClass="button" OnClick="btnXoa_Click" />
+                <asp:Button ID="btnLamMoi" runat="server" Text="Làm mới" CssClass="button" OnClick="btnLamMoi_Click" />
+                <asp:Button ID="btnTimKiem" runat="server" Text="Tìm kiếm" CssClass="button" OnClick="btnTimKiem_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ QuanLyNhanSu/qlNV.aspx.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace qlNV
+{
+    public partial class qlNV : Page
+    {
+        private string connectionString = "Your_Connection_String_Here"; // Cập nhật chuỗi kết nối của bạn
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Kiểm tra xem người dùng đã đăng nhập chưa
+            if (Session["User"] == null)
+            {
+                Response.Redirect("Login.aspx"); // Chuyển hướng đến trang đăng nhập
+            }
+
+            if (!IsPostBack)
+            {
+                // Tải dữ liệu bảng nếu cần
+                LoadNhanVien();
+            }
+        }
+
+        /*protected void previewImage()
+        {
+            // Đảm bảo file đã được tải lên và có tên
+            if (imgNhanVien.HasFile)
+            {
+                string fileName = imgNhanVien.FileName;
+                string filePath = "~/Images/" + fileName; // Đường dẫn lưu ảnh
+                string fullPath = Server.MapPath(filePath);
+
+                // Hiển thị ảnh xem trước
+                previewImg.ImageUrl = filePath;
+                previewImg.Style["display"] = "block";
+            }
+            else
+            {
+                previewImg.Style["display"] = "none";
+            }
+        }*/
+
+        protected void btnThem_Click(object sender, EventArgs e)
+        {
+            // Thêm nhân viên vào cơ sở dữ liệu
+            AddEmployee();
+            // Cập nhật GridView
+            LoadNhanVien();
+        }
+
+        protected void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            // Cập nhật thông tin nhân viên
+            UpdateEmployee();
+            // Cập nhật GridView
+            LoadNhanVien();
+        }
+
+        protected void btnXoa_Click(object sender, EventArgs e)
+        {
+            // Xóa nhân viên
+            DeleteEmployee();
+            // Cập nhật GridView
+            LoadNhanVien();
+        }
+
+        protected void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            // Xóa dữ liệu trên các ô nhập
+            ClearForm();
+        }
+
+        protected void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            // Tìm kiếm nhân viên
+            SearchEmployee();
+        }
+
+        protected void gvNhanVien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Lấy mã nhân viên được chọn từ GridView
+            GridViewRow row = gvNhanVien.SelectedRow;
+            string maNhanVien = row.Cells[0].Text; // Giả sử cột mã nhân viên ở vị trí 0
+            // Tải thông tin chi tiết của nhân viên
+            LoadEmployeeDetails(maNhanVien);
+        }
+
+        private void LoadNhanVien()
+        {
+            // Lấy dữ liệu nhân viên từ cơ sở dữ liệu và gán cho GridView
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT MaNhanVien, TenNhanVien, ChucVu, TinhTrang FROM NhanVien", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvNhanVien.DataSource = dt;
+                gvNhanVien.DataBind();
+            }
+        }
+
+        private void AddEmployee()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO NhanVien (MaNhanVien, TenNhanVien, GioiTinh, CCCD, NgayVaoLam, PhongBan, TinhTrang, DiaChi, SDT, Email, ChucVu, Luong, Anh) VALUES (@MaNhanVien, @TenNhanVien, @GioiTinh, @CCCD, @NgayVaoLam, @PhongBan, @TinhTrang, @DiaChi, @SDT, @Email, @ChucVu, @Luong, @Anh)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaNhanVien", txtMaNhanVien.Text);
+                cmd.Parameters.AddWithValue("@TenNhanVien", txtTenNhanVien.Text);
+                cmd.Parameters.AddWithValue("@GioiTinh", ddlGioiTinh.SelectedValue);
+                cmd.Parameters.AddWithValue("@CCCD", txtCCCD.Text);
+                cmd.Parameters.AddWithValue("@NgayVaoLam", DateTime.Parse(txtNgayVaoLam.Text));
+                cmd.Parameters.AddWithValue("@PhongBan", txtPhongBan.Text);
+                cmd.Parameters.AddWithValue("@TinhTrang", ddlTinhTrang.SelectedValue);
+                cmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text);
+                cmd.Parameters.AddWithValue("@SDT", txtSDT.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@ChucVu", txtChucVu.Text);
+                cmd.Parameters.AddWithValue("@Luong", decimal.Parse(txtLuong.Text));
+
+                // Xử lý ảnh
+                string anh = string.Empty;
+                if (imgNhanVien.HasFile)
+                {
+                    anh = "~/Images/" + imgNhanVien.FileName; // Đường dẫn tới ảnh
+                    imgNhanVien.SaveAs(Server.MapPath(anh));
+                }
+                cmd.Parameters.AddWithValue("@Anh", anh);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void UpdateEmployee()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE NhanVien SET TenNhanVien = @TenNhanVien, GioiTinh = @GioiTinh, CCCD = @CCCD, NgayVaoLam = @NgayVaoLam, PhongBan = @PhongBan, TinhTrang = @TinhTrang, DiaChi = @DiaChi, SDT = @SDT, Email = @Email, ChucVu = @ChucVu, Luong = @Luong, Anh = @Anh WHERE MaNhanVien = @MaNhanVien";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaNhanVien", txtMaNhanVien.Text);
+                cmd.Parameters.AddWithValue("@TenNhanVien", txtTenNhanVien.Text);
+                cmd.Parameters.AddWithValue("@GioiTinh", ddlGioiTinh.SelectedValue);
+                cmd.Parameters.AddWithValue("@CCCD", txtCCCD.Text);
+                cmd.Parameters.AddWithValue("@NgayVaoLam", DateTime.Parse(txtNgayVaoLam.Text));
+                cmd.Parameters.AddWithValue("@PhongBan", txtPhongBan.Text);
+                cmd.Parameters.AddWithValue("@TinhTrang", ddlTinhTrang.SelectedValue);
+                cmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text);
+                cmd.Parameters.AddWithValue("@SDT", txtSDT.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@ChucVu", txtChucVu.Text);
+                cmd.Parameters.AddWithValue("@Luong", decimal.Parse(txtLuong.Text));
+
+                // Xử lý ảnh
+                string anh = string.Empty;
+                if (imgNhanVien.HasFile)
+                {
+                    anh = "~/Images/" + imgNhanVien.FileName; // Đường dẫn tới ảnh
+                    imgNhanVien.SaveAs(Server.MapPath(anh));
+                }
+                cmd.Parameters.AddWithValue("@Anh", anh);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void DeleteEmployee()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM NhanVien WHERE MaNhanVien = @MaNhanVien";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaNhanVien", txtMaNhanVien.Text);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void SearchEmployee()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT MaNhanVien, TenNhanVien, ChucVu, TinhTrang FROM NhanVien WHERE TenNhanVien LIKE @TenNhanVien";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                da.SelectCommand.Parameters.AddWithValue("@TenNhanVien", "%" + txtTenNhanVien.Text + "%");
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvNhanVien.DataSource = dt;
+                gvNhanVien.DataBind();
+            }
+        }
+
+        private void LoadEmployeeDetails(string maNhanVien)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM NhanVien WHERE MaNhanVien = @MaNhanVien";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaNhanVien", maNhanVien);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtMaNhanVien.Text = reader["MaNhanVien"].ToString();
+                    txtTenNhanVien.Text = reader["TenNhanVien"].ToString();
+                    ddlGioiTinh.SelectedValue = reader["GioiTinh"].ToString();
+                    txtCCCD.Text = reader["CCCD"].ToString();
+                    txtNgayVaoLam.Text = Convert.ToDateTime(reader["NgayVaoLam"]).ToString("dd/MM/yyyy");
+                    txtPhongBan.Text = reader["PhongBan"].ToString();
+                    ddlTinhTrang.SelectedValue = reader["TinhTrang"].ToString();
+                    txtDiaChi.Text = reader["DiaChi"].ToString();
+                    txtSDT.Text = reader["SDT"].ToString();
+                    txtEmail.Text = reader["Email"].ToString();
+                    txtChucVu.Text = reader["ChucVu"].ToString();
+                    txtLuong.Text = reader["Luong"].ToString();
+                   /* previewImg.ImageUrl = reader["Anh"].ToString();
+                    previewImg.Style["display"] = "block";*/
+                }
+                reader.Close();
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtMaNhanVien.Text = string.Empty;
+            txtTenNhanVien.Text = string.Empty;
+            ddlGioiTinh.ClearSelection();
+            txtCCCD.Text = string.Empty;
+            txtNgayVaoLam.Text = string.Empty;
+            txtPhongBan.Text = string.Empty;
+            ddlTinhTrang.ClearSelection();
+            txtDiaChi.Text = string.Empty;
+            txtSDT.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtChucVu.Text = string.Empty;
+            txtLuong.Text = string.Empty;
+           /* previewImg.ImageUrl = string.Empty;
+            previewImg.Style["display"] = "none";*/
+        }
+    }
+}
