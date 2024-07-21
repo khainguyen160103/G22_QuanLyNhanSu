ad8115c22f589d302dc57f0d007b406c6be9d7de
ThienSc <21a100100357@students.hou.edu.vn>
2024-07-22 00:01:37 +0700

    Update lần 4 cho đăng nhập và mật khẩu

---
+++ QuanLyNhanSu/DangNhap/FrmDangNhap.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuanLyNhanSu.DangNhap
+{
+    public partial class FrmDangNhap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string MaQ = "";
+
+            string MS_001 = "Không để bỏ trống !";
+            string DN_02 = "Tài khoản không tồn tại !";
+            string DN_03 = "Sai mật khẩu !";
+
+            string user = txtTenTaiKhoan.Value;
+            string pass = txtMatKhau.Value;
+
+            if (!checkValidDangNhap(user, pass))
+            {
+                messageMK.InnerText = MS_001;
+            }
+            else if (login(user, pass) == 0)
+            {
+                messageTK.InnerText = DN_02;
+                messageMK.InnerText = "";
+            }
+            else if (login(user, pass) == 2)
+            {
+                messageMK.InnerText = DN_03;
+                messageTK.InnerText = "";
+            }
+            else
+            {
+                setSession(MaQ, user, pass);
+                Response.Redirect("../DoiMatKhau/FrmDoiMatKhau.aspx");
+
+            }
+
+
+
+        }
+        protected bool checkValidDangNhap(string tenTK, string mk)
+        {
+            if (tenTK.Equals("") || mk.Equals(""))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected int login(string tenTK, string mk)
+        {
+            int check = isUserNameExit(tenTK);
+            if (check == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                string qr = "SELECT COUNT(*) FROM tbl_TAIKHOAN WHERE sTaikhoan = @tenTK and sMatkhau COLLATE Latin1_General_CS_AS = @matkhau";
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                using (SqlConnection sqlcon = new SqlConnection(connectionString))
+                {
+                    sqlcon.Open();
+                    using (SqlCommand commam = new SqlCommand(qr, sqlcon))
+                    {
+                        commam.Parameters.AddWithValue("@tenTK", tenTK);
+                        commam.Parameters.AddWithValue("@matkhau", mk);
+
+                        int n = (int)commam.ExecuteScalar();
+                        if (n == 0)
+                        {
+                            return 2;
+                        }
+                        else
+                        {
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+        protected int isUserNameExit(string tenTK)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                string query = "SELECT COUNT(*) FROM tbl_TAIKHOAN WHERE sTaikhoan = @taikhoan";
+                using (SqlCommand cmd = new SqlCommand(query, sqlcon))
+                {
+                    cmd.Parameters.AddWithValue("@taikhoan", tenTK);
+
+                    int count = (int)cmd.ExecuteScalar();
+                    if (count == 0)
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return 1;
+        }
+
+        protected void setSession(string Maquyen, string tenTK, string MK)
+        {
+            string query = "SELECT FK_sMaquyen FROM tbl_TAIKHOAN WHERE sTaikhoan = @tenTK";
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@tenTK", tenTK);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Maquyen = dr.GetString(0);
+                    }
+                    dr.Close();
+                }
+            }
+            Session["MaQuyen"] = Maquyen;
+            Session["TaiKhoan"] = tenTK;
+            Session["MatKhau"] = MK;
+
+        }
+    }
+}
---
+++ QuanLyNhanSu/DoiMatKhau/FrmDoiMatKhau.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuanLyNhanSu.DoiMatKhau
+{
+    public partial class FrmDoiMatKhau : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static bool checkStreghPassword(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, "[a-z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, "[A-Z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, "[0-9]"))
+            {
+                return false;
+            }
+            return true;
+        }
+        protected string checkvalid(string MKcu, string MKmoi, string MKnhaplai)
+        {
+            string message = "";
+            string MS_001 = "Vui lòng nhập đủ thông tin";
+            string MS_002 = "Mật khẩu cần có 8 ký tự trở lên, bao gồm chữ số, chữ thường, chữ hoa";
+            string MS_003 = "Mật khẩu nhập lại không đúng";
+            string MS_004 = "Mật khẩu cũ không đúng";
+
+            string taikhoan = (string)Session["TaiKhoan"];
+
+            if (MKcu == "" || MKmoi == "" || MKnhaplai == "")
+            {
+                message = MS_001;
+            }
+            else
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                using (SqlConnection sqlcon = new SqlConnection(connectionString))
+                {
+                    sqlcon.Open();
+                    string query = "SELECT COUNT(*) FROM tbl_TAIKHOAN WHERE sTaikhoan=@taikhoan and sMatkhau = @matkhau";
+                    using (SqlCommand cmd = new SqlCommand(query, sqlcon))
+                    {
+                        cmd.Parameters.AddWithValue("@taikhoan", taikhoan);
+                        cmd.Parameters.AddWithValue("@matkhau", MKcu);
+                        int count = (int)cmd.ExecuteScalar();
+
+                        Session["check"] = count;
+                        if (count == 0)
+                        {
+                            message = MS_004;
+                        }
+                        else
+                        {
+                            if (checkStreghPassword(MKmoi) == false)
+                            {
+                                message = MS_002;
+                            }
+                            else
+                            {
+                                if (MKnhaplai != MKmoi)
+                                {
+                                    message = MS_003;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return message;
+        }
+        protected void btnDoiMatKhau_Click(object sender, EventArgs e)
+        {
+            string MS_001 = "Vui lòng nhập đủ thông tin";
+            string MS_002 = "Mật khẩu cần có 8 ký tự trở lên, bao gồm chữ số, chữ thường, chữ hoa";
+            string MS_003 = "Mật khẩu nhập lại không đúng";
+            string MS_004 = "Mật khẩu cũ không đúng";
+
+            string matkhaucu = txtMatKhauCu.Value;
+            string matkhaumoi = txtMatKhauMoi.Value;
+            string nhaplaimatkhau = txtNhapLaiMatKhauMoi.Value;
+            string taikhoan = (string)Session["TaiKhoan"];
+
+
+            string message = checkvalid(matkhaucu, matkhaumoi, nhaplaimatkhau);
+            if (message == "")
+            {
+                changePassword(taikhoan, matkhaumoi);
+            }
+            else if (message == MS_002)
+            {
+                messageMK2.InnerText = MS_002;
+                messageMK3.InnerText = "";
+            }
+            else if (message == MS_003)
+            {
+                messageMK3.InnerText = MS_003;
+                messageMK1.InnerText = "";
+                messageMK2.InnerText = "";
+            }
+            else if (message == MS_004)
+            {
+                messageMK1.InnerText = MS_004;
+                messageMK2.InnerText = "";
+                messageMK3.InnerText = "";
+            }
+            else
+            {
+                messageMK3.InnerText = MS_001;
+                messageMK2.InnerText = "";
+                messageMK1.InnerText = "";
+            }
+
+
+        }
+        protected void changePassword(string taikhoan, string matkhaumoi)
+        {
+            string connectString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection sqlcon = new SqlConnection(connectString))
+            {
+                sqlcon.Open();
+                string query = "UPDATE tbl_TAIKHOAN set sMatkhau = @matkhaumoi where sTaikhoan = @taikhoan";
+                using (SqlCommand cmd = new SqlCommand(query, sqlcon))
+                {
+                    cmd.Parameters.AddWithValue("@matkhaumoi", matkhaumoi);
+                    cmd.Parameters.AddWithValue("@taikhoan", taikhoan);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+            Response.Redirect("FrmDangNhap.aspx");
+        }
+    }
+}
