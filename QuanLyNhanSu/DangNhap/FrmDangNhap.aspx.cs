e41097d52c2868d890fd41a55ff19b09153c711e
ThienSc <21a100100357@students.hou.edu.vn>
2024-07-22 00:40:04 +0700

    update hàm isUserNameExit

--- QuanLyNhanSu/DangNhap/FrmDangNhap.aspx.cs
+++ QuanLyNhanSu/DangNhap/FrmDangNhap.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,5 +15,26 @@ namespace QuanLyNhanSu.DangNhap
         {
 
         }
+        protected int isUserNameExit(string tenTK)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                string query = "SELECT COUNT(*) FROM tbl_TAIKHOAN WHERE sTaikhoan = @taikhoan";
+                using (SqlCommand cmd = new SqlCommand(query, sqlcon))
+                {
+                    cmd.Parameters.AddWithValue("@taikhoan", tenTK);
+
+                    int count = (int)cmd.ExecuteScalar();
+                    if (count == 0)
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return 1;
+        }
+
     }
 }
